dc5575626cdf1fa6dcda3314b383217ca7ed0032
Phong <chauthaiphong03@gmail.com>
2022-11-30 20:11:02 +0700

    ...

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var connection = require('./Dbconnection');
 var routes = require('./routes');
+const cors = require("cors")
 
 var app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
   
-var server = app.listen(3000, function() {
+var server = app.listen(4200, function() {
   console.log('Server listening on port ' + server.address().port);
 });
 
+
 module.exports = app;
+app.use(cors())
 app.use('/',routes);
